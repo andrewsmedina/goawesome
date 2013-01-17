8dedbf1fb44b6e1e79de33d88175f1b6ebf2f812
Andrews Medina <andrewsmedina@gmail.com>
2013-01-17 01:15:12 -0200

    Parsing operators in lexer.

--- lexer.go
+++ lexer.go
@@ -9,8 +9,14 @@ type token map[string]interface{}
 func tokenizer(code []byte) []token {
 	var tokens []token
 	numberRegex, _ := regexp.Compile("([0-9]+)")
+	operatorRegex, _ := regexp.Compile(`(\|\||&&|==|!=|<=|>=)`)
 	if number := numberRegex.Find(code); len(number) > 0 {
 		tokens = append(tokens, token{"type": "NUMBER", "value": string(number)})
+	} else if operator := string(operatorRegex.Find(code)); len(operator) > 0 {
+		tokens = append(tokens, token{"type": operator, "value": operator})
+	} else {
+		t := string(code)
+		tokens = append(tokens, token{"type": t, "value": t})
 	}
 	return tokens
 }
--- lexer_test.go
+++ lexer_test.go
@@ -14,3 +14,8 @@ var _ = Suite(&S{})
 func (s *S) TestNumber(c *C) {
 	c.Assert(tokenizer([]byte("1")), DeepEquals, []token{{"type": "NUMBER", "value": "1"}})
 }
+
+func (s *S) TestOperator(c *C) {
+	c.Assert(tokenizer([]byte("+")), DeepEquals, []token{{"type": "+", "value": "+"}})
+	c.Assert(tokenizer([]byte("||")), DeepEquals, []token{{"type": "||", "value": "||"}})
+}
